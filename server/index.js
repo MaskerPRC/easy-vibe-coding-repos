d32df34844f09c3f08c8f4043c5fc8e48707b55a
Code Sync Service <code-sync@world-app.local>
2025-11-09 22:54:30 +0800

    Auto sync: 2025-11-09T14:54:30.029Z

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs/promises';
 import axios from 'axios';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const DATA_FILE = path.join(__dirname, 'data.json');
