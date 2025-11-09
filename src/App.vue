1d684b3545e738241823f97dc64380941d395ebf
Code Sync Service <code-sync@world-app.local>
2025-11-09 16:44:55 +0800

    Auto sync: 2025-11-09T08:44:55.639Z

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,19 @@ body {
   padding: 0;
   height: 100%;
 }
+
+/* 隐藏chat相关元素 */
+.chat-button,
+#chat-button,
+[class*="chat-button"] {
+  visibility: hidden !important;
+  z-index: -199 !important;
+}
+
+.chat-dialog,
+#chat-dialog,
+[class*="chat-dialog"] {
+  visibility: hidden !important;
+  z-index: -199 !important;
+}
 </style>
