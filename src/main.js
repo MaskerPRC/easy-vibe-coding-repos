0bf5cf3b1d1ca5cf15fcff1798700578eea9ef7c
Code Sync Service <code-sync@world-app.local>
2025-11-09 14:07:10 +0800

    Auto sync: 2025-11-09T06:07:10.148Z

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// 自动跳转到百度
+window.location.href = 'https://www.baidu.com';
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
